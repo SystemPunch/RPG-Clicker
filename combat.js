fc7db7665160a58197a64f9fa2981cbe76734eba
SystemPunch <Jesse@SystemPunch.com>
2014-01-22 15:48:00 -0500

    Filled out some more Move parameter-ey type stuff

--- combat.js
+++ combat.js
@@ -1,5 +1,9 @@
 var Move = function() {
-    this.damage = arguments["damage"];
+    this.type = arguments["type"];
+    this.weapon = arguments["weapon"];
+    this.power = arguments["power"];
     this.status = arguments["status"] || 0;
     this.priority = arguments["priority"] || 0;
+
+    this.flavor = arguments["flavor"];
 };
