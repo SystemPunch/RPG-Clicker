de8661051a07e1874ca344590332753c5f5ac6f6
SystemPunch <Jesse@SystemPunch.com>
2014-01-23 18:32:36 -0500

    Created moves.js

--- combat.js
+++
@@ -1,9 +0,0 @@
-var Move = function() {
-    this.type = arguments["type"];
-    this.weapon = arguments["weapon"];
-    this.power = arguments["power"];
-    this.status = arguments["status"] || 0;
-    this.priority = arguments["priority"] || 0;
-
-    this.flavor = arguments["flavor"];
-};
