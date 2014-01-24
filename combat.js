5d0d7cda8d1aa7a021f9a1bd9cc565ea88330fed
SystemPunch <Jesse@SystemPunch.com>
2014-01-23 21:53:32 -0500

    Pseudocode for combat

---
+++ combat.js
@@ -0,0 +1,22 @@
+/*
+
+Player selects monster to fight
+    show combat pane with skill selection
+
+    while battle isn't over
+        player selects skill
+            select move for monster
+
+            if one is paralyzed, make other go first
+
+            otherwise compare priority, speed, etc. to determine order
+
+            do damage, print messages, check for victory, etc.
+                increase proficiency as needed
+
+            do any other status stuff like poison, burn, etc.
+
+    if battle is over
+        show appropriate messages, give rewards
+
+ */
