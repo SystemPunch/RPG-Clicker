8264e553088d03ad7614bb44c8d32581d32d9c22
SystemPunch <Jesse@SystemPunch.com>
2014-02-03 11:21:49 -0500

    0.3.12, updateCharacterPanel on game load

--- main.js
+++ main.js
@@ -28,12 +28,13 @@ $(function () {
 
     loadGame();
     updateGame();
+    updateCharacterPanel();
     checkVersion();
 
     bottomNotify("This is a VERY early version of the game. It may be riddled with bugs, and saves might break. If you find that your game isn't working properly, try resetting your save. If that doesn't work, please send me a bug report. This message will disappear after 15 seconds.", "warning", 15000);
 });
 
-var VERSION = "0.3.11";
+var VERSION = "0.3.12";
 
 var settings = {
     autoSave: "ON",
